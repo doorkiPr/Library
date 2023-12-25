50b39cc0c4f514746f706b1c310f41d7641e941c
doorkiPr <boumarafchakib7@gmail.com>
2023-12-25 03:42:55 +0100

    FIX:fixed typo

--- script.js
+++ script.js
@@ -8,23 +8,22 @@ function Book(title,author,pages,read) {
     this.pages = pages;
     this.read = read;
 }
-
 function addBooktoLibrary(book) {
     myLibrary.push(book);
 }
 
 function displayBook(book) {
-    const card =  Document.createElement("div");
+    const card =  document.createElement("div");
     card.classList.toggle("card");
-    const title = Document.createElement("div");
+    const title = document.createElement("div");
     card.classList.toggle("cardText");
-    const author = Document.createElement("div");
+    const author = document.createElement("div");
     card.classList.toggle("cardText");
-    const pages = Document.createElement("div");
+    const pages = document.createElement("div");
     card.classList.toggle("cardText");
-    const read = Document.createElement("button");
+    const read = document.createElement("button");
     read.classList.toggle("readBtn");
-    const deleteBook = Document.createElement("button");
+    const deleteBook = document.createElement("button");
     deleteBook.classList.toggle("deleteBtn");
   
     read.textContent = book.read ? "Already read" : "Not read yet";
@@ -38,5 +37,4 @@ function displayBook(book) {
     card.appendChild(pages);
     card.appendChild(read);
     card.appendChild(deleteBook);  
-  
-  }
+}
