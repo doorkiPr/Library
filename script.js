66ebd45637b43fc1589fb5f143928a4d7107edf8
doorkiPr <boumarafchakib7@gmail.com>
2023-12-25 03:31:49 +0100

    FIX:removed uppercase letter from document

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const libraryDiv = Document.querySelector('.library');
-const addbtn = Document.querySelector('.addBook');
+const libraryDiv = document.querySelector('.library');
+const addbtn = document.querySelector('.addBook');
 const myLibrary = [];
 
 function Book(title,author,pages,read) {
@@ -11,4 +11,32 @@ function Book(title,author,pages,read) {
 
 function addBooktoLibrary(book) {
     myLibrary.puh(book);
+}
+
+function displayBook(book) {
+  const card =  Document.createElement("div");
+  card.classList.toggle("card");
+  const title = Document.createElement("div");
+  card.classList.toggle("cardText");
+  const author = Document.createElement("div");
+  card.classList.toggle("cardText");
+  const pages = Document.createElement("div");
+  card.classList.toggle("cardText");
+  const read = Document.createElement("button");
+  read.classList.toggle("readBtn");
+  const deleteBook = Document.createElement("button");
+  deleteBook.classList.toggle("deleteBtn");
+
+  read.textContent = book.read ? "Already read" : "Not read yet";
+  deleteBook.textContent = "delete"  
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages + " pages.";
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+  card.appendChild(deleteBook);  
+
 }
