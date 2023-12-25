ef372cea6e075d417aa62f0c8f41b23cbd2ba62d
doorkiPr <boumarafchakib7@gmail.com>
2023-12-25 03:32:07 +0100

    ADD:add displayBook function

--- script.js
+++ script.js
@@ -14,29 +14,29 @@ function addBooktoLibrary(book) {
 }
 
 function displayBook(book) {
-  const card =  Document.createElement("div");
-  card.classList.toggle("card");
-  const title = Document.createElement("div");
-  card.classList.toggle("cardText");
-  const author = Document.createElement("div");
-  card.classList.toggle("cardText");
-  const pages = Document.createElement("div");
-  card.classList.toggle("cardText");
-  const read = Document.createElement("button");
-  read.classList.toggle("readBtn");
-  const deleteBook = Document.createElement("button");
-  deleteBook.classList.toggle("deleteBtn");
-
-  read.textContent = book.read ? "Already read" : "Not read yet";
-  deleteBook.textContent = "delete"  
-  title.textContent = book.title;
-  author.textContent = book.author;
-  pages.textContent = book.pages + " pages.";
-
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages);
-  card.appendChild(read);
-  card.appendChild(deleteBook);  
-
-}
+    const card =  Document.createElement("div");
+    card.classList.toggle("card");
+    const title = Document.createElement("div");
+    card.classList.toggle("cardText");
+    const author = Document.createElement("div");
+    card.classList.toggle("cardText");
+    const pages = Document.createElement("div");
+    card.classList.toggle("cardText");
+    const read = Document.createElement("button");
+    read.classList.toggle("readBtn");
+    const deleteBook = Document.createElement("button");
+    deleteBook.classList.toggle("deleteBtn");
+  
+    read.textContent = book.read ? "Already read" : "Not read yet";
+    deleteBook.textContent = "delete"  
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages + " pages.";
+  
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    card.appendChild(deleteBook);  
+  
+  }
