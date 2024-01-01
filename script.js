32a0a26cb7166097226ab956753aeb74b1fe4ee2
doorkiPr <boumarafchakib7@gmail.com>
2024-01-01 12:12:55 +0100

    FEAT: refractor code using class instead of factory function

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const libraryDiv = document.querySelector('.library');
 const addbtn = document.querySelector('.addBook');
 const dialog = document.querySelector('dialog');
 const myform = document.querySelector('form');
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 const myLibrary = [];
 
